c3d7acac9b3a6ddecacfef7ab5cad54b3624f0da
Muhammad Radwan <Marvinwork001@gmail.com>
2026-02-07 01:22:26 +0300

    Part One

---
+++ Js/main.js
@@ -0,0 +1,30 @@
+const bars = document.querySelector(".bars");
+const sections = document.querySelector(".sections");
+
+bars.addEventListener("click", (el) => {
+  el.stopPropagation();
+  bars.classList.toggle("clicked");
+  sections.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!bars.contains(e.target) && !sections.contains(e.target)) {
+    bars.classList.remove("clicked");
+    sections.classList.remove("active");
+  }
+});
+
+const text = "عالم من أحدث الملابس والاكسسوارت";
+const el = document.getElementById("typewriter");
+let i = 0;
+
+function typeWriter() {
+  el.textContent = text.slice(0, i);
+  i++;
+
+  if (i <= text.length) {
+    setTimeout(typeWriter, 50);
+  }
+}
+
+typeWriter();
